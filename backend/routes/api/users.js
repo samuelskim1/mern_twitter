aca23a38b044fed7f7c59d73474cce40ff623e90
Your Name <Your Email>
2023-04-16 14:10:13 -0400

    complete Password JWT Setup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
-const { loginUser } = require('../../config/passport');
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -76,6 +78,22 @@ router.post('/login', async(req, res, next) => {
     return res.json( await loginUser(user));
   })(req, res, next);
 })
+
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    // In development, allow React server to gain access to the CSRF token
+    // whenever the current user information is first loaded into the
+    // React application
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email
+  });
+  })
 
 
 module.exports = router;
