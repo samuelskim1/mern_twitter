146f734970ea525989e3489a8bed78a9875dc7a7
Your Name <Your Email>
2023-04-16 16:55:03 -0400

    complete react setup

---
+++ backend/models/Tweet.js
@@ -0,0 +1,18 @@
+const { text } = require('express');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tweetSchema = new Schema({
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    text: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Tweet', tweetSchema);
