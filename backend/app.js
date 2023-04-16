00664b0b1e6f9b67277bb455b7dd03e3bc9fc3a1
Your Name <Your Email>
2023-04-15 23:02:30 -0400

    complete Express Error Handling

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 
 const cors = require('cors');
 const csurf = require('csurf');
+const debug = require('debug');
 const { isProduction } = require('./config/keys');
 
 const usersRouter = require('./routes/api/users');
@@ -42,5 +43,39 @@ app.use(
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
