93425cafa8f0d733779a4b562442bcf84f47c82a
Your Name <Your Email>
2023-04-15 22:56:12 -0400

    complete Express Server Security

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
+const csrfRouter = require('./routes/api/csrf');
 
 const app = express();
 
@@ -12,7 +17,30 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+// ADD THIS SECURITY MIDDLEWARE
+// Security Middleware
+if (!isProduction) {
+    // Enable CORS only in development because React will be on the React
+    // development server (http://localhost:3000). (In production, the Express 
+    // server will serve the React files statically.)
+    app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
+
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
+app.use('/api/csrf', csrfRouter);
 
 module.exports = app;
---
+++ backend/routes/api/csrf.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+const { isProduction } = require('../../config/keys');
+
+/* GET csrfs listing. */
+router.get('/', function (req, res, next) {
+    res.json({
+        message: "GET /api/csrfs"
+    })
+
+});
+
+if (!isProduction) {
+    // In development, allow developers to access the CSRF token to test the
+  // server endpoints in Postman.
+    router.get('/restore', function (req, res) {
+        const csrfToken = req.csrfToken();
+        res.status(200).json({
+            'CSRF-Token': csrfToken
+        });
+    });
+}
+
+module.exports = router;
