e887edad6c8beabdde9a84f70e1c742e0f170cdb
Your Name <Your Email>
2023-04-15 22:30:28 -0400

    complete Express Routing

--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a user resource after doing npm run dev');
-});
-
-module.exports = router;
